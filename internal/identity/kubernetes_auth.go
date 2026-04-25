b6bb51de2930a66fc2e1ea57d1d1399d7157e939
Moritz Johner <beller.moritz@googlemail.com>
2026-04-25 12:47:59 +0000

    fix: use native sdk auth for managed kubernetes

--- internal/identity/kubernetes_auth.go
+++ internal/identity/kubernetes_auth.go
@@ -3,14 +3,27 @@ package identity
 import (
 	"context"
 	"encoding/base64"
-	"encoding/json"
 	"fmt"
-	"os/exec"
+	"net/http"
 	"strings"
+	"time"
 
+	"cloud.google.com/go/container/apiv1"
+	"cloud.google.com/go/container/apiv1/containerpb"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservice/armcontainerservice/v6"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	v4 "github.com/aws/aws-sdk-go-v2/aws/signer/v4"
+	awsconfig "github.com/aws/aws-sdk-go-v2/config"
+	awseks "github.com/aws/aws-sdk-go-v2/service/eks"
+	awseksTypes "github.com/aws/aws-sdk-go-v2/service/eks/types"
+	awssts "github.com/aws/aws-sdk-go-v2/service/sts"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
-	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
 
 	"github.com/moolen/aegis/internal/config"
 )
@@ -28,6 +41,26 @@ var loadInCluster = rest.InClusterConfig
 var loadEKS = defaultLoadEKS
 var loadGKE = defaultLoadGKE
 var loadAKS = defaultLoadAKS
+
+type managedClusterConnection struct {
+	Host   string
+	CAData []byte
+}
+
+type eksRESTConfigDeps struct {
+	describeCluster func(ctx context.Context, region string, clusterName string) (managedClusterConnection, error)
+	tokenSource     func(ctx context.Context, region string, clusterName string) (oauth2.TokenSource, error)
+}
+
+type gkeRESTConfigDeps struct {
+	describeCluster func(ctx context.Context, project string, location string, clusterName string) (managedClusterConnection, error)
+	tokenSource     func(ctx context.Context) (oauth2.TokenSource, error)
+}
+
+type aksRESTConfigDeps struct {
+	loadKubeconfig func(ctx context.Context, subscriptionID string, resourceGroup string, clusterName string) ([]byte, error)
+	tokenSource    func(ctx context.Context, rawKubeconfig []byte) (oauth2.TokenSource, error)
+}
 
 func defaultKubernetesAuthDeps() kubernetesAuthDeps {
 	return kubernetesAuthDeps{
@@ -86,85 +119,394 @@ func defaultLoadKubeconfig(path string, kubeContext string) (*rest.Config, error
 }
 
 func defaultLoadEKS(ctx context.Context, region string, clusterName string) (*rest.Config, error) {
-	kubeconfig, err := runCommandForConfig(ctx, "aws", "eks", "update-kubeconfig", "--dry-run", "--name", clusterName, "--region", region)
-	if err != nil {
-		return nil, err
-	}
-
-	return clientcmd.RESTConfigFromKubeConfig(kubeconfig)
+	return buildEKSRESTConfig(ctx, region, clusterName, eksRESTConfigDeps{
+		describeCluster: defaultDescribeEKSCluster,
+		tokenSource:     defaultEKSTokenSource,
+	})
 }
 
 func defaultLoadGKE(ctx context.Context, project string, location string, clusterName string) (*rest.Config, error) {
-	description, err := runCommandForConfig(
-		ctx,
-		"gcloud", "container", "clusters", "describe", clusterName,
-		"--location", location,
-		"--project", project,
-		"--format=json",
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	var cluster struct {
-		Endpoint   string `json:"endpoint"`
-		MasterAuth struct {
-			ClusterCACertificate string `json:"clusterCaCertificate"`
-		} `json:"masterAuth"`
-	}
-	if err := json.Unmarshal(description, &cluster); err != nil {
-		return nil, err
-	}
-
-	caData, err := base64.StdEncoding.DecodeString(cluster.MasterAuth.ClusterCACertificate)
-	if err != nil {
-		return nil, err
-	}
-
-	return &rest.Config{
-		Host: "https://" + cluster.Endpoint,
-		TLSClientConfig: rest.TLSClientConfig{
-			CAData: caData,
-		},
-		ExecProvider: &clientcmdapi.ExecConfig{
-			APIVersion:  "client.authentication.k8s.io/v1beta1",
-			Command:     "gke-gcloud-auth-plugin",
-			InstallHint: "install gke-gcloud-auth-plugin to authenticate to GKE clusters",
-		},
-	}, nil
+	return buildGKERestConfig(ctx, project, location, clusterName, gkeRESTConfigDeps{
+		describeCluster: defaultDescribeGKECluster,
+		tokenSource:     defaultGKETokenSource,
+	})
 }
 
 func defaultLoadAKS(ctx context.Context, subscriptionID string, resourceGroup string, clusterName string) (*rest.Config, error) {
-	kubeconfig, err := runCommandForConfig(
-		ctx,
-		"az", "aks", "get-credentials",
-		"--subscription", subscriptionID,
-		"--resource-group", resourceGroup,
-		"--name", clusterName,
-		"--file", "-",
-		"--format", "exec",
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	return clientcmd.RESTConfigFromKubeConfig(kubeconfig)
+	return buildAKSRESTConfig(ctx, subscriptionID, resourceGroup, clusterName, aksRESTConfigDeps{
+		loadKubeconfig: defaultLoadAKSKubeconfig,
+		tokenSource:    defaultAKSTokenSource,
+	})
 }
 
 func normalizeKubernetesAuthProvider(provider string) string {
 	return strings.ToLower(strings.TrimSpace(provider))
 }
 
-func runCommandForConfig(ctx context.Context, name string, args ...string) ([]byte, error) {
-	cmd := exec.CommandContext(ctx, name, args...)
-
-	output, err := cmd.Output()
+func buildEKSRESTConfig(ctx context.Context, region string, clusterName string, deps eksRESTConfigDeps) (*rest.Config, error) {
+	clusterConn, err := deps.describeCluster(ctx, region, clusterName)
 	if err != nil {
-		if exitErr, ok := err.(*exec.ExitError); ok {
-			return nil, fmt.Errorf("%s %s: %s", name, strings.Join(args, " "), strings.TrimSpace(string(exitErr.Stderr)))
-		}
 		return nil, err
 	}
 
-	return output, nil
+	tokenSource, err := deps.tokenSource(ctx, region, clusterName)
+	if err != nil {
+		return nil, err
+	}
+
+	return newBearerTokenRESTConfig(clusterConn, tokenSource), nil
 }
+
+func buildGKERestConfig(ctx context.Context, project string, location string, clusterName string, deps gkeRESTConfigDeps) (*rest.Config, error) {
+	clusterConn, err := deps.describeCluster(ctx, project, location, clusterName)
+	if err != nil {
+		return nil, err
+	}
+
+	tokenSource, err := deps.tokenSource(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return newBearerTokenRESTConfig(clusterConn, tokenSource), nil
+}
+
+func buildAKSRESTConfig(ctx context.Context, subscriptionID string, resourceGroup string, clusterName string, deps aksRESTConfigDeps) (*rest.Config, error) {
+	rawKubeconfig, err := deps.loadKubeconfig(ctx, subscriptionID, resourceGroup, clusterName)
+	if err != nil {
+		return nil, err
+	}
+
+	restCfg, err := clientcmd.RESTConfigFromKubeConfig(rawKubeconfig)
+	if err != nil {
+		return nil, err
+	}
+
+	tokenSource, err := deps.tokenSource(ctx, rawKubeconfig)
+	if err != nil {
+		return nil, err
+	}
+
+	return applyBearerTokenSource(restCfg, tokenSource), nil
+}
+
+func defaultDescribeEKSCluster(ctx context.Context, region string, clusterName string) (managedClusterConnection, error) {
+	awsCfg, err := awsconfig.LoadDefaultConfig(ctx, awsconfig.WithRegion(region))
+	if err != nil {
+		return managedClusterConnection{}, err
+	}
+
+	client := awseks.NewFromConfig(awsCfg)
+	output, err := client.DescribeCluster(ctx, &awseks.DescribeClusterInput{Name: aws.String(clusterName)})
+	if err != nil {
+		return managedClusterConnection{}, err
+	}
+
+	return managedClusterConnectionFromEKS(output.Cluster)
+}
+
+func defaultEKSTokenSource(ctx context.Context, region string, clusterName string) (oauth2.TokenSource, error) {
+	awsCfg, err := awsconfig.LoadDefaultConfig(ctx, awsconfig.WithRegion(region))
+	if err != nil {
+		return nil, err
+	}
+
+	return oauth2.ReuseTokenSource(nil, &eksTokenSource{
+		credentials:      awsCfg.Credentials,
+		signer:           v4.NewSigner(),
+		endpointResolver: awssts.NewDefaultEndpointResolverV2(),
+		region:           region,
+		clusterName:      clusterName,
+	}), nil
+}
+
+func defaultDescribeGKECluster(ctx context.Context, project string, location string, clusterName string) (managedClusterConnection, error) {
+	client, err := container.NewClusterManagerClient(ctx)
+	if err != nil {
+		return managedClusterConnection{}, err
+	}
+	defer client.Close()
+
+	cluster, err := client.GetCluster(ctx, &containerpb.GetClusterRequest{
+		Name: gkeClusterResourceName(project, location, clusterName),
+	})
+	if err != nil {
+		return managedClusterConnection{}, err
+	}
+
+	return managedClusterConnectionFromGKE(cluster)
+}
+
+func defaultGKETokenSource(ctx context.Context) (oauth2.TokenSource, error) {
+	return google.DefaultTokenSource(ctx, container.DefaultAuthScopes()...)
+}
+
+func defaultLoadAKSKubeconfig(ctx context.Context, subscriptionID string, resourceGroup string, clusterName string) ([]byte, error) {
+	credential, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := armcontainerservice.NewManagedClustersClient(subscriptionID, credential, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	format := armcontainerservice.FormatExec
+	response, err := client.ListClusterUserCredentials(ctx, resourceGroup, clusterName, &armcontainerservice.ManagedClustersClientListClusterUserCredentialsOptions{
+		Format: &format,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	if len(response.Kubeconfigs) == 0 || len(response.Kubeconfigs[0].Value) == 0 {
+		return nil, fmt.Errorf("aks cluster %s returned no kubeconfig", clusterName)
+	}
+
+	return append([]byte(nil), response.Kubeconfigs[0].Value...), nil
+}
+
+func defaultAKSTokenSource(ctx context.Context, rawKubeconfig []byte) (oauth2.TokenSource, error) {
+	serverID, tenantID, err := aksTokenRequestParameters(rawKubeconfig)
+	if err != nil {
+		return nil, err
+	}
+
+	credential, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return oauth2.ReuseTokenSource(nil, &azureOAuth2TokenSource{
+		ctx:        ctx,
+		credential: credential,
+		options: policy.TokenRequestOptions{
+			Scopes:   []string{serverID + "/.default"},
+			TenantID: tenantID,
+		},
+	}), nil
+}
+
+func managedClusterConnectionFromEKS(cluster *awseksTypes.Cluster) (managedClusterConnection, error) {
+	if cluster == nil {
+		return managedClusterConnection{}, fmt.Errorf("eks cluster response was empty")
+	}
+	if cluster.CertificateAuthority == nil || strings.TrimSpace(aws.ToString(cluster.CertificateAuthority.Data)) == "" {
+		return managedClusterConnection{}, fmt.Errorf("eks cluster %q did not include certificate authority data", aws.ToString(cluster.Name))
+	}
+	if strings.TrimSpace(aws.ToString(cluster.Endpoint)) == "" {
+		return managedClusterConnection{}, fmt.Errorf("eks cluster %q did not include an endpoint", aws.ToString(cluster.Name))
+	}
+
+	caData, err := base64.StdEncoding.DecodeString(aws.ToString(cluster.CertificateAuthority.Data))
+	if err != nil {
+		return managedClusterConnection{}, err
+	}
+
+	return managedClusterConnection{
+		Host:   normalizeKubernetesHost(aws.ToString(cluster.Endpoint)),
+		CAData: caData,
+	}, nil
+}
+
+func managedClusterConnectionFromGKE(cluster *containerpb.Cluster) (managedClusterConnection, error) {
+	if cluster == nil {
+		return managedClusterConnection{}, fmt.Errorf("gke cluster response was empty")
+	}
+	if strings.TrimSpace(cluster.GetEndpoint()) == "" {
+		return managedClusterConnection{}, fmt.Errorf("gke cluster %q did not include an endpoint", cluster.GetName())
+	}
+	if cluster.GetMasterAuth() == nil || strings.TrimSpace(cluster.GetMasterAuth().GetClusterCaCertificate()) == "" {
+		return managedClusterConnection{}, fmt.Errorf("gke cluster %q did not include certificate authority data", cluster.GetName())
+	}
+
+	caData, err := base64.StdEncoding.DecodeString(cluster.GetMasterAuth().GetClusterCaCertificate())
+	if err != nil {
+		return managedClusterConnection{}, err
+	}
+
+	return managedClusterConnection{
+		Host:   normalizeKubernetesHost(cluster.GetEndpoint()),
+		CAData: caData,
+	}, nil
+}
+
+func newBearerTokenRESTConfig(clusterConn managedClusterConnection, tokenSource oauth2.TokenSource) *rest.Config {
+	return applyBearerTokenSource(&rest.Config{
+		Host: clusterConn.Host,
+		TLSClientConfig: rest.TLSClientConfig{
+			CAData: append([]byte(nil), clusterConn.CAData...),
+		},
+	}, tokenSource)
+}
+
+func applyBearerTokenSource(restCfg *rest.Config, tokenSource oauth2.TokenSource) *rest.Config {
+	cfg := rest.CopyConfig(restCfg)
+	previousWrap := cfg.WrapTransport
+	cfg.ExecProvider = nil
+	cfg.AuthProvider = nil
+	cfg.BearerToken = ""
+	cfg.BearerTokenFile = ""
+	cfg.WrapTransport = func(rt http.RoundTripper) http.RoundTripper {
+		if previousWrap != nil {
+			rt = previousWrap(rt)
+		}
+		return bearerTokenRoundTripper{
+			base:        rt,
+			tokenSource: tokenSource,
+		}
+	}
+
+	return cfg
+}
+
+func gkeClusterResourceName(project string, location string, clusterName string) string {
+	return fmt.Sprintf("projects/%s/locations/%s/clusters/%s", project, location, clusterName)
+}
+
+func aksTokenRequestParameters(rawKubeconfig []byte) (string, string, error) {
+	cfg, err := clientcmd.Load(rawKubeconfig)
+	if err != nil {
+		return "", "", err
+	}
+
+	contextName := cfg.CurrentContext
+	if strings.TrimSpace(contextName) == "" {
+		for name := range cfg.Contexts {
+			contextName = name
+			break
+		}
+	}
+
+	contextCfg, ok := cfg.Contexts[contextName]
+	if !ok || contextCfg == nil {
+		return "", "", fmt.Errorf("aks kubeconfig did not include a usable current context")
+	}
+
+	authInfo, ok := cfg.AuthInfos[contextCfg.AuthInfo]
+	if !ok || authInfo == nil || authInfo.Exec == nil {
+		return "", "", fmt.Errorf("aks kubeconfig did not include exec auth configuration")
+	}
+
+	serverID := execArgValue(authInfo.Exec.Args, "--server-id")
+	if strings.TrimSpace(serverID) == "" {
+		return "", "", fmt.Errorf("aks kubeconfig did not include --server-id in exec args")
+	}
+
+	return serverID, execArgValue(authInfo.Exec.Args, "--tenant-id"), nil
+}
+
+func execArgValue(args []string, flag string) string {
+	for i := 0; i < len(args); i++ {
+		if args[i] == flag && i+1 < len(args) {
+			return args[i+1]
+		}
+		if strings.HasPrefix(args[i], flag+"=") {
+			return strings.TrimPrefix(args[i], flag+"=")
+		}
+	}
+
+	return ""
+}
+
+func normalizeKubernetesHost(host string) string {
+	trimmed := strings.TrimSpace(host)
+	if strings.HasPrefix(trimmed, "https://") || strings.HasPrefix(trimmed, "http://") {
+		return trimmed
+	}
+	return "https://" + trimmed
+}
+
+type bearerTokenRoundTripper struct {
+	base        http.RoundTripper
+	tokenSource oauth2.TokenSource
+}
+
+func (rt bearerTokenRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
+	token, err := rt.tokenSource.Token()
+	if err != nil {
+		return nil, err
+	}
+
+	cloned := req.Clone(req.Context())
+	cloned.Header.Set("Authorization", "Bearer "+token.AccessToken)
+	base := rt.base
+	if base == nil {
+		base = http.DefaultTransport
+	}
+	return base.RoundTrip(cloned)
+}
+
+type azureOAuth2TokenSource struct {
+	ctx        context.Context
+	credential azcore.TokenCredential
+	options    policy.TokenRequestOptions
+}
+
+func (s *azureOAuth2TokenSource) Token() (*oauth2.Token, error) {
+	token, err := s.credential.GetToken(s.ctx, s.options)
+	if err != nil {
+		return nil, err
+	}
+
+	return &oauth2.Token{
+		AccessToken: token.Token,
+		Expiry:      token.ExpiresOn,
+	}, nil
+}
+
+type eksTokenSource struct {
+	credentials      aws.CredentialsProvider
+	signer           *v4.Signer
+	endpointResolver awssts.EndpointResolverV2
+	region           string
+	clusterName      string
+}
+
+func (s *eksTokenSource) Token() (*oauth2.Token, error) {
+	credentials, err := s.credentials.Retrieve(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	endpoint, err := s.endpointResolver.ResolveEndpoint(context.Background(), awssts.EndpointParameters{
+		Region:            aws.String(s.region),
+		UseGlobalEndpoint: aws.Bool(false),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest(http.MethodGet, endpoint.URI.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	query := req.URL.Query()
+	query.Set("Action", "GetCallerIdentity")
+	query.Set("Version", "2011-06-15")
+	query.Set("X-Amz-Expires", "60")
+	req.URL.RawQuery = query.Encode()
+	req.Header.Set("x-k8s-aws-id", s.clusterName)
+
+	signedURL, _, err := s.signer.PresignHTTP(
+		context.Background(),
+		credentials,
+		req,
+		emptyPayloadSHA256,
+		awssts.ServiceID,
+		s.region,
+		time.Now().UTC(),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &oauth2.Token{
+		AccessToken: "k8s-aws-v1." + base64.RawURLEncoding.EncodeToString([]byte(signedURL)),
+		Expiry:      time.Now().Add(14 * time.Minute),
+	}, nil
+}
+
+const emptyPayloadSHA256 = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
--- internal/identity/kubernetes_auth_test.go
+++ internal/identity/kubernetes_auth_test.go
@@ -3,9 +3,12 @@ package identity
 import (
 	"context"
 	"errors"
+	"io"
+	"net/http"
 	"strings"
 	"testing"
 
+	"golang.org/x/oauth2"
 	"k8s.io/client-go/rest"
 
 	"github.com/moolen/aegis/internal/config"
@@ -225,4 +228,213 @@ func TestBuildKubernetesRESTConfigPropagatesLoaderErrors(t *testing.T) {
 	if !errors.Is(err, loadErr) {
 		t.Fatalf("error = %v, want wrapped loader error", err)
 	}
+}
+
+func TestBuildEKSRESTConfigUsesClusterMetadataAndBearerTokenSource(t *testing.T) {
+	ctx := context.WithValue(context.Background(), struct{}{}, "ctx")
+	clusterConn := managedClusterConnection{
+		Host:   "https://eks.example",
+		CAData: []byte("eks-ca"),
+	}
+	tokenSource := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: "eks-token"})
+
+	var gotDescribeCtx context.Context
+	var gotTokenCtx context.Context
+	cfg, err := buildEKSRESTConfig(ctx, "eu-central-1", "cluster-a", eksRESTConfigDeps{
+		describeCluster: func(ctx context.Context, region string, clusterName string) (managedClusterConnection, error) {
+			gotDescribeCtx = ctx
+			if region != "eu-central-1" {
+				t.Fatalf("describeCluster region = %q, want eu-central-1", region)
+			}
+			if clusterName != "cluster-a" {
+				t.Fatalf("describeCluster clusterName = %q, want cluster-a", clusterName)
+			}
+			return clusterConn, nil
+		},
+		tokenSource: func(ctx context.Context, region string, clusterName string) (oauth2.TokenSource, error) {
+			gotTokenCtx = ctx
+			if region != "eu-central-1" {
+				t.Fatalf("tokenSource region = %q, want eu-central-1", region)
+			}
+			if clusterName != "cluster-a" {
+				t.Fatalf("tokenSource clusterName = %q, want cluster-a", clusterName)
+			}
+			return tokenSource, nil
+		},
+	})
+	if err != nil {
+		t.Fatalf("buildEKSRESTConfig() error = %v", err)
+	}
+	if gotDescribeCtx != ctx {
+		t.Fatal("describeCluster context was not forwarded")
+	}
+	if gotTokenCtx != ctx {
+		t.Fatal("tokenSource context was not forwarded")
+	}
+	if cfg.Host != clusterConn.Host {
+		t.Fatalf("config host = %q, want %q", cfg.Host, clusterConn.Host)
+	}
+	if string(cfg.TLSClientConfig.CAData) != string(clusterConn.CAData) {
+		t.Fatalf("config CAData = %q, want %q", cfg.TLSClientConfig.CAData, clusterConn.CAData)
+	}
+	if cfg.ExecProvider != nil {
+		t.Fatalf("config ExecProvider = %#v, want nil", cfg.ExecProvider)
+	}
+	authHeader := authorizationHeader(t, cfg)
+	if authHeader != "Bearer eks-token" {
+		t.Fatalf("authorization header = %q, want Bearer eks-token", authHeader)
+	}
+}
+
+func TestBuildGKERestConfigUsesClusterMetadataAndBearerTokenSource(t *testing.T) {
+	requestCtx := context.WithValue(context.Background(), struct{}{}, "ctx")
+	clusterConn := managedClusterConnection{
+		Host:   "https://gke.example",
+		CAData: []byte("gke-ca"),
+	}
+	tokenSource := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: "gke-token"})
+
+	cfg, err := buildGKERestConfig(requestCtx, "prod-project", "europe-west1", "cluster-b", gkeRESTConfigDeps{
+		describeCluster: func(ctx context.Context, project string, location string, clusterName string) (managedClusterConnection, error) {
+			if ctx != requestCtx {
+				t.Fatal("describeCluster context mismatch")
+			}
+			if project != "prod-project" || location != "europe-west1" || clusterName != "cluster-b" {
+				t.Fatalf("describeCluster args = %q/%q/%q", project, location, clusterName)
+			}
+			return clusterConn, nil
+		},
+		tokenSource: func(ctx context.Context) (oauth2.TokenSource, error) {
+			if ctx != requestCtx {
+				t.Fatal("tokenSource context mismatch")
+			}
+			return tokenSource, nil
+		},
+	})
+	if err != nil {
+		t.Fatalf("buildGKERestConfig() error = %v", err)
+	}
+	if cfg.Host != clusterConn.Host {
+		t.Fatalf("config host = %q, want %q", cfg.Host, clusterConn.Host)
+	}
+	if string(cfg.TLSClientConfig.CAData) != string(clusterConn.CAData) {
+		t.Fatalf("config CAData = %q, want %q", cfg.TLSClientConfig.CAData, clusterConn.CAData)
+	}
+	if cfg.ExecProvider != nil {
+		t.Fatalf("config ExecProvider = %#v, want nil", cfg.ExecProvider)
+	}
+	authHeader := authorizationHeader(t, cfg)
+	if authHeader != "Bearer gke-token" {
+		t.Fatalf("authorization header = %q, want Bearer gke-token", authHeader)
+	}
+}
+
+func TestBuildAKSRESTConfigUsesReturnedKubeconfigAndAmbientTokenSource(t *testing.T) {
+	requestCtx := context.WithValue(context.Background(), struct{}{}, "ctx")
+	kubeconfig := []byte(`
+apiVersion: v1
+clusters:
+- cluster:
+    certificate-authority-data: YWtzLWNh
+    server: https://aks.example
+  name: cluster
+contexts:
+- context:
+    cluster: cluster
+    user: user
+  name: context
+current-context: context
+kind: Config
+users:
+- name: user
+  user:
+    exec:
+      apiVersion: client.authentication.k8s.io/v1beta1
+      command: kubelogin
+`)
+	tokenSource := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: "aks-token"})
+
+	cfg, err := buildAKSRESTConfig(requestCtx, "sub-123", "rg-platform", "cluster-c", aksRESTConfigDeps{
+		loadKubeconfig: func(ctx context.Context, subscriptionID string, resourceGroup string, clusterName string) ([]byte, error) {
+			if ctx != requestCtx {
+				t.Fatal("loadKubeconfig context mismatch")
+			}
+			if subscriptionID != "sub-123" || resourceGroup != "rg-platform" || clusterName != "cluster-c" {
+				t.Fatalf("loadKubeconfig args = %q/%q/%q", subscriptionID, resourceGroup, clusterName)
+			}
+			return kubeconfig, nil
+		},
+		tokenSource: func(ctx context.Context, rawKubeconfig []byte) (oauth2.TokenSource, error) {
+			if ctx != requestCtx {
+				t.Fatal("tokenSource context mismatch")
+			}
+			if string(rawKubeconfig) != string(kubeconfig) {
+				t.Fatal("tokenSource received unexpected kubeconfig")
+			}
+			return tokenSource, nil
+		},
+	})
+	if err != nil {
+		t.Fatalf("buildAKSRESTConfig() error = %v", err)
+	}
+	if cfg.Host != "https://aks.example" {
+		t.Fatalf("config host = %q, want https://aks.example", cfg.Host)
+	}
+	if string(cfg.TLSClientConfig.CAData) != "aks-ca" {
+		t.Fatalf("config CAData = %q, want aks-ca", cfg.TLSClientConfig.CAData)
+	}
+	if cfg.ExecProvider != nil {
+		t.Fatalf("config ExecProvider = %#v, want nil", cfg.ExecProvider)
+	}
+	if cfg.AuthProvider != nil {
+		t.Fatalf("config AuthProvider = %#v, want nil", cfg.AuthProvider)
+	}
+	authHeader := authorizationHeader(t, cfg)
+	if authHeader != "Bearer aks-token" {
+		t.Fatalf("authorization header = %q, want Bearer aks-token", authHeader)
+	}
+}
+
+func TestBuildAKSRESTConfigPropagatesKubeconfigErrors(t *testing.T) {
+	loadErr := errors.New("aks unavailable")
+
+	_, err := buildAKSRESTConfig(context.Background(), "sub-123", "rg-platform", "cluster-c", aksRESTConfigDeps{
+		loadKubeconfig: func(context.Context, string, string, string) ([]byte, error) {
+			return nil, loadErr
+		},
+	})
+	if !errors.Is(err, loadErr) {
+		t.Fatalf("error = %v, want wrapped kubeconfig error", err)
+	}
+}
+
+func authorizationHeader(t *testing.T, cfg *rest.Config) string {
+	t.Helper()
+
+	transport := cfg.WrapTransport(roundTripFunc(func(req *http.Request) (*http.Response, error) {
+		return &http.Response{
+			StatusCode: http.StatusOK,
+			Body:       io.NopCloser(strings.NewReader("ok")),
+			Request:    req,
+		}, nil
+	}))
+
+	req, err := http.NewRequest(http.MethodGet, "https://example.com", nil)
+	if err != nil {
+		t.Fatalf("http.NewRequest() error = %v", err)
+	}
+
+	resp, err := transport.RoundTrip(req)
+	if err != nil {
+		t.Fatalf("RoundTrip() error = %v", err)
+	}
+	defer resp.Body.Close()
+
+	return resp.Request.Header.Get("Authorization")
+}
+
+type roundTripFunc func(*http.Request) (*http.Response, error)
+
+func (fn roundTripFunc) RoundTrip(req *http.Request) (*http.Response, error) {
+	return fn(req)
 }
